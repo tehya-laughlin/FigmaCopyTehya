659ece0936d3b1f42330dde3d7746a27b61eef54
tehya-laughlin <tehyanlaugh.shop@gmail.com>
2023-10-17 20:37:11 -0600

    Initial Commit

---
+++ FigmaCopyTehya/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FigmaCopyTehya
+//
+//  Created by Tehya Laughlin on 10/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FigmaCopyTehya/FigmaCopyTehyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaCopyTehyaApp.swift
+//  FigmaCopyTehya
+//
+//  Created by Tehya Laughlin on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct FigmaCopyTehyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
